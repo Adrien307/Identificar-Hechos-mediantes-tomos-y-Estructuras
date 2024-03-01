b4471e6d00eaf22f5fa05cded33c8553f3bc4c18
unknown <lpze.adrian.lijeron.re@unifranz.edu.bo>
2024-03-01 15:37:46 -0400

    Identificar Hechos mediantes Átomos y Estructuras

---
+++ universidad.pl
@@ -0,0 +1,32 @@
+mario.
+andres.
+arturica.
+programacion1.
+redes2.
+inteligencia_artificial.
+carla.
+magenta.
+giovana.
+elena.
+
+ensena(programacion1, mario).
+ensena(programacion1, arturica).
+
+ensena(redes2, mario).
+ensena(redes2, andres).
+
+ensena(inteligencia_artificial, arturica).
+
+pasa_clases(programacion1, magenta).
+
+pasa_clases(redes2, carla).
+pasa_clases(redes2, magenta).
+pasa_clases(redes2, giovana).
+
+pasa_clases(inteligencia_artificial, carla).
+pasa_clases(inteligencia_artificial, magenta).
+pasa_clases(inteligencia_artificial, giovana).
+pasa_clases(inteligencia_artificial, elena).
+
+
+
